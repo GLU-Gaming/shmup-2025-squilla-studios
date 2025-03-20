fda598a92e7bb1e8a73a7ecfa5cb6c01df1f44af
RV-Works <robinavalkenburg@gmail.com>
2025-03-20 09:54:56 +0100

    Player finished
    The player cannot leave the camera's view anymore

--- Assets/Developers/Robina/Script/PlayerMovement.cs
+++ Assets/Developers/Robina/Script/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
 
 
-
     void FixedUpdate()
     {
         // Get the input from the player
@@ -16,8 +15,25 @@ public class PlayerMovement : MonoBehaviour
         // Move the player
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        // Keep the player in the screen
+        if (transform.position.x > 8.3f)
+        {
+            transform.position = new Vector3(8.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -8.3f)
+        {
+            transform.position = new Vector3(-8.3f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > 7.6f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 7.6f); 
+        }
+        else if (transform.position.z < -4.31f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -4.31f);
+        }
     }
- 
+ //7,6 -4,31
  
    
         
--- Assets/Developers/Robina/Script/Projectile.cs
+++ Assets/Developers/Robina/Script/Projectile.cs
@@ -1,7 +1,11 @@
-using UnityEngine;
+ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private Rigidbody rb;
+    [SerializeField] private float speed = 10f;
+    //[SerializeField] private GameObject explosionPrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
