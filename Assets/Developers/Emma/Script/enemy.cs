01cadee1ed4eafd8928a29e1319a81d1e360f108
Emma vizee <candyfloof@outlook.com>
2025-03-25 11:21:33 +0100

    busy with wave spawner and edited the enemy shoot timer

--- Assets/Developers/Emma/Script/enemy.cs
+++ Assets/Developers/Emma/Script/enemy.cs
@@ -7,12 +7,12 @@ public class enemy : MonoBehaviour
     [SerializeField] private Transform bulletSpawnPoint;
     [SerializeField] private GameObject bulletPrefab;
     private float shootTimer = 2f;
-    [SerializeField] private float shootCooldown = 0.5f;
+    [SerializeField] private float shootCooldown;
     public Vector3 pos;
 
     void Start()
     {
-        
+        shootCooldown = Random.Range(1.0f, 1.7f);
     }
      
     
