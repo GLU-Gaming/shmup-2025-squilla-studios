3cc4024fe82d7dce6b8764679a4454758d307b95
Emma vizee <candyfloof@outlook.com>
2025-04-02 09:28:56 +0200

    player

--- Assets/Developers/Gameplay/Enemies/enemykamikaze.cs
+++ Assets/Developers/Gameplay/Enemies/enemykamikaze.cs
@@ -1,40 +1,36 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Audio;
 
 public class enemykamikaze : MonoBehaviour
 {
-    [SerializeField] private GameObject target;
-    private Rigidbody enemyRb;
-    [SerializeField] private int bulletSpeed;
-    [SerializeField] private float sphereRadius;
-
+    //[SerializeField] private GameObject target;
+    //private Rigidbody enemyRb;
+    //[SerializeField] private int Speed;
+    //[SerializeField] private float sphereRadius;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject enemy;
+    public Vector3 targetPosition;
+    public float moveSpeed;
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
         
+
     }
 
    
     void Update()
     {
-        //if (Physics.CheckSphere(transform.position, sphereRadius) && gameObject.CompareTag("Player"))
-        //{
-        //    Debug.Log("AAAAAAAAAAAA *explosion*");
-        //}
-        transform.LookAt(target.transform);
-        enemyRb.linearVelocity = transform.forward * bulletSpeed;
+        /*transform.LookAt(target.transform);
+        enemyRb.linearVelocity = transform.forward * Speed;*/
+        Vector3 directionToMove = targetPosition - transform.position;
+        directionToMove = directionToMove.normalized * Time.deltaTime * moveSpeed;
+        float maxDistance = Vector3.Distance(transform.position, targetPosition);
+        transform.position = transform.position + Vector3.ClampMagnitude(directionToMove, maxDistance);
     }
 
-    void checkIfInSphere()
-    {
-        if (Physics.CheckSphere(transform.position, sphereRadius) && gameObject.CompareTag("Player"))
-        {
-            Debug.Log("AAAAAAAAAAAA *explosion*");
-        }
-    }
     
-
-    //make enemy look at player then follow player
-    //stop if in radius have a countdown before exploding 
-}
+    
+    //when spawned go the the location of where the player was 
+   }
